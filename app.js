e1af94d047c278a20e288450bfe355e6bafea57c
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-11-07 16:05:15 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -11,25 +11,25 @@ $(document).keypress(function (event) {
 
 function makeSound(key) {
     if (key === "w") {
-        const crash = new Audio("sounds/crash.mp3");
+        const crash = new Audio("soundsJQuery/crash.mp3");
         crash.play();
     } else if (key === "a") {
-        const kick = new Audio("sounds/kick-bass.mp3");
+        const kick = new Audio("soundsJQuery/kick-bass.mp3");
         kick.play();
     } else if (key === "s") {
-        const snare = new Audio("sounds/snare.mp3");
+        const snare = new Audio("soundsJQuery/snare.mp3");
         snare.play();
     } else if (key === "d") {
-        const tom1 = new Audio("sounds/tom-1.mp3");
+        const tom1 = new Audio("soundsJQuery/tom-1.mp3");
         tom1.play();
     } else if (key === "j") {
-        const tom2 = new Audio("sounds/tom-2.mp3");
+        const tom2 = new Audio("soundsJQuery/tom-2.mp3");
         tom2.play();
     } else if (key === "k") {
-        const tom3 = new Audio("sounds/tom-3.mp3");
+        const tom3 = new Audio("soundsJQuery/tom-3.mp3");
         tom3.play();
     } else if (key === "l") {
-        const tom4 = new Audio("sounds/tom-4.mp3");
+        const tom4 = new Audio("soundsJQuery/tom-4.mp3");
         tom4.play();
     }
 }
